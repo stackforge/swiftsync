87eb8b21e69728827841b28353f0d56d7d63ab5c
Chmouel Boudjnah <chmouel@chmouel.com>
2013-03-26 14:53:37 +0100

    Call the proper method after refactoring was done.

--- bin/syncit.py
+++ bin/syncit.py
@@ -4,8 +4,8 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from sync.accounts import sync_accounts
+import sync.accounts
 
 
 if __name__ == '__main__':
-    sync_accounts()
+    sync.accounts.main()
